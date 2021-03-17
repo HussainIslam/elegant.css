c9d33244369c8e23cac215ebed6609fc623e8ecf
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-03-17 19:45:12 -0400

    added marketing page

---
+++ packages/r3site/pages/MarketingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MarketingPage = () => {
+    return (
+        <div>
+            This is the Marketing Page
+        </div>
+    )
+}
+
+export default MarketingPage
